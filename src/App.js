649b9a901bcd1b0015a9db2eb6ae3739a73c2162
Reppypurpose <54214913+Reppypurpose@users.noreply.github.com>
2019-12-04 01:23:17 -0800

    Routing initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Home from './Home';
+import About from './About';
+import Us from './Us'
+import Contact from './Contact';
+import Portofolio from './Portofolio';
+import Service from './Service'
+// import '/App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+          <div className="App">
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+    <Link to="/service">Contact</Link>
+    <Link to="/us">Us</Link>
+    <Link to="/Portofolio">Portofolio</Link>
+    
     </div>
+
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
+    <Route exact path="/us" component={Us} />
+    <Route exact path="/service" component={Service} />
+    <Route exact path="/portofolio" component={Portofolio} />
+    </Router>
   );
 }
 
